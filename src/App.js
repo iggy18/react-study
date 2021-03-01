7222556aa9c44e3ab52337b9515b6cdc6fa9782f
seth mcfeeters <seth.mcfeeters@gmail.com>
2021-02-28 18:58:35 -0800

    added props and state to files. can change state on click

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/person';
 
 class App extends Component {
+  state = {
+    people: [
+      {name: 'John', age: 34},
+      {name: 'chad', age: 36},
+      {name: 'Bob', age:42}
+    ]
+  }
+
+  switchNameHandler = () => {
+    this.setState({
+      people: [
+      {name: 'Bill', age: 1},
+      {name: 'Choad', age: 2},
+      {name: 'Billy', age:3}
+      ]
+    });
+  }
+
   render() {
-    // return (
-    //   <div className="App">
-    //     <h1>still working?</h1>
-    //   </div>
-    // );
-    return React.createElement('div', null, 'h1', '')
+    return (
+      <div className="App">
+        <h1>still working?</h1>
+        <p>yup, still working</p>
+        <button onClick={this.switchNameHandler}>Swich Name</button>
+        <Person name={this.state.people[0].name} age={this.state.people[0].age}/>
+        <Person name={this.state.people[1].name} age={this.state.people[1].age}>my hobby is: skiing.</Person>
+        <Person name={this.state.people[2].name} age={this.state.people[2].age}/>
+      </div>
+    );
   }
 }
 
